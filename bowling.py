bce9c6e5477f4dc86a6e8250359896e0484f48ea
mateBedok <mate.bedok@gmail.com>
2017-07-31 16:23:42 +0200

    bad name fixed, get value simplified

--- bowling.py
+++ bowling.py
@@ -2,44 +2,33 @@ def score(game):
     result = 0
     frame = 1
     in_first_half = True
-    for i in range(len(game)):
-        if game[i] == '/':
+    for game_round in range(len(game)):
+        if game[game_round] == '/':
             result += 10 - last
         else:
-            result += get_value(game[i])
+            result += get_value(game[game_round])
         # if not in_first_half:
             # frame += 1
-        if frame < 10  and get_value(game[i]) == 10:
-            if game[i] == '/':
-                result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
+        if frame < 10  and get_value(game[game_round]) == 10:
+            if game[game_round] == '/':
+                result += get_value(game[game_round+1])
+            elif game[game_round] == 'X' or game[game_round] == 'x':
+                result += get_value(game[game_round+1])
+                if game[game_round+2] == '/':
+                    result += 10 - get_value(game[game_round+1])
                 else:
-                    result += get_value(game[i+2])
-        last = get_value(game[i])
+                    result += get_value(game[game_round+2])
+        last = get_value(game[game_round])
         if not in_first_half:
             frame += 1
         if in_first_half == True:
             in_first_half = False
         else:
             in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
+        if game[game_round] == 'X' or game[game_round] == 'x':
             in_first_half = True
             frame += 1
     return result
 
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
-        return int(char)
-    elif char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
-        return 10
-    elif char == '-':
-        return 0
-    else:
-        raise ValueError()
+    return int(char) if char in "123456789" else 10 if char.upper() in "X/" else 0 if char == "-" else ValueError
